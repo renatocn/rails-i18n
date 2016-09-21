b800d122389af8bc7670f31b90a5d9f6ffa7db1f
Renato Nitta <renatocn@gmail.com>
2016-09-21 16:56:06 -0300

    simplify the rule

--- rails/pluralization/ar.rb
+++ rails/pluralization/ar.rb
@@ -1,32 +1,3 @@
-module RailsI18n
-  module Pluralization
-    module Arabic
-      def self.rule
-        lambda do |n|
-          n ||= 0
-          mod100 = n % 100
+require 'rails_i18n/common_pluralizations/other'
 
-          if n == 0
-            :zero
-          elsif n == 1
-            :one
-          elsif n == 2
-            :two
-          elsif (3..10).to_a.include?(mod100)
-            :few
-          elsif (11..99).to_a.include?(mod100)
-            :many
-          else
-            :other
-          end
-        end
-      end
-    end
-  end
-end
-
-{ :ar => {
-    :'i18n' => {
-      :plural => {
-        :keys => [:zero, :one, :two, :few, :many, :other],
-        :rule => RailsI18n::Pluralization::Arabic.rule }}}}
+::RailsI18n::Pluralization::Other.with_locale(:ar)
